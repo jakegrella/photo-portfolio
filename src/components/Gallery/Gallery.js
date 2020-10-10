3761360045e3b4f4b08f240d25a3764a9f09bc5d
Jake Grella <jgrella13@gmail.com>
2020-10-10 07:45:22 -0500

    added link to gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import { StyledGallery } from './Gallery.styled'
 
 const Gallery = (props) => {
     return (
-        <StyledGallery>
+		<StyledGallery>
+		<Link to='/order'><h2 className="banner">click to order senior night photos</h2></Link>
             {props.photos && (
 				<>
 					{props.photos.map((photo) => (

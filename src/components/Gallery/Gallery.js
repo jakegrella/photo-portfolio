f984b78c6a03eaa6953759c503754632512bbe5c
Jake Grella <jgrella13@gmail.com>
2020-10-10 20:43:16 -0500

    fixed favicon and centered homepage banner button

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,7 +6,7 @@ import { StyledGallery } from './Gallery.styled'
 const Gallery = (props) => {
     return (
 		<StyledGallery>
-		<Link to='/order'><h2 className="banner">click to order senior night photos</h2></Link>
+		<h2 className="banner"><Link to='/order'>click to order senior night photos</Link></h2>
             {props.photos && (
 				<>
 					{props.photos.map((photo) => (
--- src/components/Gallery/Gallery.styled.js
+++ src/components/Gallery/Gallery.styled.js
@@ -10,6 +10,17 @@ export const StyledGallery = styled.div`
 		object-fit: cover;
 		padding: 0.5% 0.5%;
 	}
+	.banner {
+		flex-basis: 100%;
+		text-align: center;
+		margin-bottom: 2%;
+	}
+	.banner Link {
+		color: ${pr => pr.theme.primaryDark};
+		&:hover {
+			color: ${pr => pr.theme.primaryHover};
+		}
+	}
 	@media (max-width: ${({ theme }) => theme.tablet}) {
 		margin: 5% 5% 0;
 		img {
